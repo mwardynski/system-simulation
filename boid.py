063cf42f1ea46ced0804cbc3154b27a3d943d235
Bartłomiej Kruczek <bkruczekk@student.agh.edu.pl>
2024-03-27 21:40:09 +0100

    Update boid.py

--- boid.py
+++ boid.py
@@ -16,10 +16,9 @@ class Boid:
     def draw(self, board):
         pygame.draw.circle(board, "blue", (int(self.x), int(self.y)), 3)
 
-    def applyInflus(self, influs):
-        for influ in influs:
-            self.vx += influ[0]
-            self.vy += influ[1]
+    def applyInflus(self, influ_vx, influ_vy):
+        self.vx += influ_vx
+        self.vy += influ_vy
 
     def update(self):
         self.scale_velocity()
